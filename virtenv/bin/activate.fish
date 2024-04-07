39d62648a1948fbbac74d5fe187623d82d8509fa
barcodin9 <barcodingss@gmail.com>
2024-04-07 18:47:24 +0100

    Recreate virtual environment, initial import of stripe

--- virtenv/bin/activate.fish
+++ virtenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/barra/CI-PP5/CI-PP5/virtenv"
+set -gx VIRTUAL_ENV "/Users/barra/CI-PP5/virtenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
