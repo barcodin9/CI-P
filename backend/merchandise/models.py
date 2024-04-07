9efc3ac8c6e31ed0512e86e7c83dfee060e9fc29
barcodin9 <barcodingss@gmail.com>
2024-04-08 00:22:05 +0100

    Initial setup of db models for store.

--- backend/merchandise/models.py
+++ backend/merchandise/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    Name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.Name
+    
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+class Clothing(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.Name
+
+class Media(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.Name
+
+class Misc(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.Name
