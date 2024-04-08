505211a1cf3d79ac15a9063547a2e9bf8eaa2800
barcodin9 <barcodingss@gmail.com>
2024-04-08 01:37:34 +0100

    Add products and categories to admin panel to be edited - adjust naming convention in db.

--- backend/merchandise/models.py
+++ backend/merchandise/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.Name
+        return self.name
     
     
     def get_friendly_name(self):
@@ -18,9 +18,10 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.Name
+        return self.name
 
