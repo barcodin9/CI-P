093a81e7004db7eca1d78e3867d0c12d2c4efeb3
barcodin9 <barcodingss@gmail.com>
2024-04-10 17:40:23 +0100

    Basic cart styling added - cart btn added to index.

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path, views
-from .views import cart_summary
+from django.urls import path 
+from .views import cart_summary, cart_add, cart_delete, cart_update
 
 urlpatterns = [
-   path('', views.cart_summary, name='cart_summary'),
-   path('add/', views.cart_add, name='cart_add'),
-   path('delete/', views.cart_delete, name='cart_delete'),
-   path('update/', views.cart_update, name='cart_update'),
-   
-
+   path('', cart_summary, name='cart_summary'),
+   path('add/', cart_add, name='cart_add'),
+   path('delete/', cart_delete, name='cart_delete'),
+   path('update/', cart_update, name='cart_update'),
 ]
-
