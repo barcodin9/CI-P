e619432d600eefe900b9e88bef23bbad007872f1
barcodin9 <barcodingss@gmail.com>
2024-04-10 21:04:26 +0100

    Add to cart button will correctly identify the product selected - further cart functionality to be implemented.

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from merchandise.models import Product
+from django.http import JsonResponse
+
 
 # Create your views here.
 def cart_summary(request):
     return render(request, 'cart_summary.html')
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    # test POST
+    if request.POST.get('action') == 'post':
+        #get product related
+        product_id = int(request.POST.get('product_id'))
+        #product in db
+        product = get_object_or_404(Product, id=product_id)
+        #save to session
+        cart.add(product=product,)
+
+        #return response
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
+
+
+
 
 def cart_delete(request):
     pass
--- backend/merchandise/urls.py
+++ backend/merchandise/urls.py
@@ -1,8 +1,9 @@
 
-from django.urls import path 
+from django.urls import path, include
 from merchandise.views import get_all_products, merchandise
 
 urlpatterns = [
     path('merch/', get_all_products, name='all_products'),
     path('merch/', merchandise, name='merch'),
+    path('cart/', include('cart.urls')),    
 ]
