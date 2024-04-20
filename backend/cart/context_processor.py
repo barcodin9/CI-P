f54c381fdf006201b308c5af3292611a7793e242
barcodin9 <barcodingss@gmail.com>
2024-04-20 14:40:09 +0100

    Add basic checkout functionality.

--- backend/cart/context_processor.py
+++ backend/cart/context_processor.py
@@ -15,9 +15,9 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
 
-    for item_id, item_data in bag.items():
+    for item_id, item_data in cart.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
--- backend/checkout/views.py
+++ backend/checkout/views.py
@@ -10,22 +10,40 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     print("Debug - Cart Contents:", cart)
-    
     if not cart:
         messages.error(request, "There's currently nothing in your basket.")
         return redirect('merch')
     
     current_cart = cart_contents(request)
+    print("Current cart:", current_cart)
     total = current_cart['grand_total']
-    stripe_total = round(total * 100)
+    print("Total from cart:", total)
+
+    stripe_total = round(Decimal(total) * 100)
+
+    if stripe_total < 50:  
+            messages.error(request, "The total amount must be at least €0.50")
+
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
 
     if request.method == 'POST':
         order_form = OrderForm(request.POST)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            order.total = total
+            order.save()
             # Process payment and order here (pseudo-code)
             # clear the session cart after payment
             request.session['cart'] = {}
@@ -40,19 +58,22 @@ def checkout(request):
         {'product': product, 'quantity': cart[str(product.id)], 'subtotal': product.price * cart[str(product.id)]}
         for product in products
     ]
-    
     subtotal = sum(Decimal(item['subtotal']) for item in cart_products)
     delivery_cost = subtotal * Decimal('0.10')
     grand_total = subtotal + delivery_cost
 
+    if not stripe_public_key:
+         messages.warning(request, 'Stripe public key is missing. \ Did you forget to set it in your environment?')
+
+    template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'cart_products': cart_products,
         'subtotal': subtotal,
         'delivery_cost': delivery_cost,
         'grand_total': grand_total,
-        'stripe_public_key': 'pk_test_51P2aj3Rv8bNUIMLrIvltTuLuoBRklBZnXEd4s6aUo3hvsuKxZGoWr9poabPAuGXIbSrsJMBRRhDZyO3OAyT6cEjf00k2bTP9cp',
-        'client_secret': 'test_client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     
 
