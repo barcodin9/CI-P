0d7f7660dd7266e3bb824c1f39d8ab366d2accf0
barcodin9 <barcodingss@gmail.com>
2024-05-06 06:27:56 +0100

    Debug false and static fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # settings.py
 MEDIA_URL = '/media/'
