15e78a7fd9397c1cbb25d53a5e1b19a4fab9e604
barcodin9 <barcodingss@gmail.com>
2024-05-06 03:53:10 +0100

    Add favicon

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'TESTKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['the-crypt-91228a836777.herokuapp.com', '127.0.0.1']
 
