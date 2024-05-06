64066b0e107ac06445a891c0478d1ddb1d52811b
barcodin9 <barcodingss@gmail.com>
2024-05-06 05:41:53 +0100

    Code cleanup and debug off.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'TESTKEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-crypt-91228a836777.herokuapp.com', '127.0.0.1']
 
